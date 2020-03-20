42a302291d90cf5bdc15f8fce1b8c8eccc520f2e
dhernquist <david.hernquist@turner.com>
2020-03-20 17:41:50 -0400

    added datasources

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+
+const { RESTDataSource, RequestOtions } = require("apollo-datasource-rest");
 const { ApolloServer, gql } = require("apollo-server");
 
 // A schema is a collection of type definitions (hence "typeDefs")
@@ -20,28 +23,45 @@ const typeDefs = gql`
   }
 `;
 
-const orders = [
-  {
-    id: "Harry Potter and the Chamber of Secrets",
-    email: "J.K. Rowling"
-  },
-  {
-    id: "Jurassic Park",
-    email: "Michael Crichton"
+// class PersonalizationAPI extends RESTDataSource {
+//   willSendRequest(request) {
+//     request.headers.set("Authorization", `Basic ${process.env.BASE64_AUTH}`);
+//   }
+// }
+
+class ShopifyAPI extends RESTDataSource {
+  constructor() {
+    super();
+    const { SHOP_API, SHOP_PASSWORD } = process.env;
+    this.baseURL = `https://${SHOP_API}:${SHOP_PASSWORD}@lucky-lovely-shop.myshopify.com/admin/api/2020-01/`;
   }
-];
+
+  async getOrders() {
+    return this.get(`orders.json`);
+  }
+}
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    orders: () => orders
+    orders: async (_source, _, { dataSources }) => {
+      return dataSources.shopifyAPI.getOrders();
+    }
   }
 };
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      shopifyAPI: new ShopifyAPI()
+    };
+  }
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
